95d5fac4b7574447f1673740d0dd9eda0fc146ce
odastrand <126185047+odastrand@users.noreply.github.com>
2023-04-19 10:37:10 +0200

    Update routes.py

--- Capp/carbon_app/routes.py
+++ Capp/carbon_app/routes.py
@@ -1,4 +1,6 @@
 from flask import render_template, Blueprint
+from capp.models import Transport
+from flask_login import login_required
 
 carbon_app=Blueprint('carbon_app',__name__)
 
@@ -7,10 +9,11 @@ def carbon_app_home():
     return render_template('carbon_app/carbon_app.html', title='carbon_app')
 
 @carbon_app.route('/carbon_app/new_entry')
+@login_required
 def new_entry_home():
     return render_template('carbon_app/new_entry.html', title='new_entry')
 
 @carbon_app.route('/carbon_app/your_data')
+@login_required
 def your_data_home():
     return render_template('carbon_app/your_data.html', title='your_data')
-    
